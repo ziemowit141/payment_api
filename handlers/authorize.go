3f7455265aab1df5b7a2a46d66be343fb95e19d2
ziemowit141 <ziemowit141@gmail.com>
2022-02-08 18:05:31 +0100

    Authorize endpoint handler

---
+++ handlers/authorize.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/ziemowit141/payment_api/algorithms"
+	"github.com/ziemowit141/payment_api/handlers/io_structures"
+	"gorm.io/gorm"
+)
+
+type Authorize struct {
+	db *gorm.DB
+}
+
+func NewAuthorizeHandler(db *gorm.DB) *Authorize {
+	return &Authorize{db}
+}
+
+func (a *Authorize) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		a.postAuthorize(rw, r)
+	}
+
+	rw.WriteHeader(http.StatusNotImplemented)
+}
+
+func (a *Authorize) postAuthorize(rw http.ResponseWriter, r *http.Request) {
+	authReq := io_structures.NewAuthorizationRequest(r.Body)
+	session := algorithms.NewSession(a.db)
+
+	status := session.AuthorizeWithCardDetails(authReq)
+	if status != "SUCCESS" {
+		authRes := &io_structures.AuthorizationResponse{
+			Uid:      "",
+			Status:   status,
+			Balance:  0.0,
+			Currency: "NaN"}
+		authRes.ToJSON(rw)
+		return
+	}
+
+	transactionId := session.AddTransaction(authReq.Amount)
+
+	authRes := &io_structures.AuthorizationResponse{
+		Uid:      transactionId,
+		Status:   status,
+		Balance:  session.CardNetBalance(),
+		Currency: authReq.Currency}
+	authRes.ToJSON(rw)
+}
