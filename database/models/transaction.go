fbbb40921a6043ddd4affd059da51ed8dcdbaa4b
ziemowit141 <piotrstroz@Piotrs-MacBook-Pro.local>
2022-02-08 14:56:15 +0100

    Add Transaction model

---
+++ database/models/transaction.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type Transaction struct {
+	gorm.Model
+	Id               string    `gorm:"primaryKey"`
+	Amount           float32
+	Currency         string
+	CreditCardNumber string
+	Captures         []Capture `gorm:"foreignKey:TransactionId;references:Id;constraint:OnDelete:CASCADE"`
+	Refunds          []Refund  `gorm:"foreignKey:TransactionId;references:Id;constraint:OnDelete:CASCADE"`
+}
+
+func (a Transaction) String() string {
+	return fmt.Sprintf("Transaction<%s %s %v %s>", a.Id, a.CreditCardNumber, a.Amount, a.Currency)
+}
